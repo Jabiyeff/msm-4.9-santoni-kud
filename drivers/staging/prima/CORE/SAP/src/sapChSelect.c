bf0c64b4c2ce3cbdda13611fe48cccf1d09390a5
Jabiyeff <cebiyevanar@gmail.com>
2020-12-02 13:39:15 +0400

    prima: CORE: fix -Wswitch-unreachable warning ../drivers/staging/prima/CORE/SAP/src/sapChSelect.c: 'sapComputeSpectWeight' işlevinde: ../drivers/staging/prima/CORE/SAP/src/sapChSelect.c:1538:50: UYARI: statement will never be executed [-Wswitch-unreachable]  1538 |                                 tSapSpectChInfo *pExtSpectCh = NULL;
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- drivers/staging/prima/CORE/SAP/src/sapChSelect.c
+++ drivers/staging/prima/CORE/SAP/src/sapChSelect.c
@@ -1535,7 +1535,6 @@ void sapComputeSpectWeight( tSapChSelSpectInfo* pSpectInfoParams,
                         case eHT_CHANNEL_WIDTH_40MHZ: //HT40
                             switch( secondaryChannelOffset)
                             {
-                                tSapSpectChInfo *pExtSpectCh = NULL;
                                 case PHY_DOUBLE_CHANNEL_LOW_PRIMARY: // Above the Primary Channel
                                     pExtSpectCh = (pSpectCh + 1);
                                     if( pExtSpectCh != NULL &&
