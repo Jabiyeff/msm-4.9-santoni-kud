b5d9ef2063f7f6e2ee2aa1fbd5b9c8e0da550fe8
Jabiyeff <cebiyevanar@gmail.com>
2021-02-14 18:29:55 +0400

    Revert "ion: Use spin_lock after 73d6760d3505bb11c5d13dbd9f20cccc75c6a4cd"
    This reverts commit 1bfc5d1f3acb26289498acce6ab823123b7f1d5c.

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -228,15 +228,15 @@ static bool ion_page_pool_isolate(struct page *page, isolate_mode_t mode)
 		return false;
 	pool = mapping->private_data;
 
-	spin_lock(&pool->lock);
+	mutex_lock(&pool->mutex);
 	/* could be removed from the cache pool and thus become unmovable */
 	if (!__PageMovable(page)) {
-		spin_unlock(&pool->lock);
+		mutex_unlock(&pool->mutex);
 		return false;
 	}
 
 	if (unlikely(!test_bit(ION_PAGE_CACHE, &page->private))) {
-		spin_unlock(&pool->lock);
+		mutex_unlock(&pool->mutex);
 		return false;
 	}
 
@@ -245,7 +245,7 @@ static bool ion_page_pool_isolate(struct page *page, isolate_mode_t mode)
 		pool->high_count--;
 	else
 		pool->low_count--;
-	spin_unlock(&pool->lock);
+	mutex_unlock(&pool->mutex);
 
 	return true;
 }
@@ -266,7 +266,7 @@ static int ion_page_pool_migrate(struct address_space *mapping,
 	__ClearPageMovable(page);
 	ClearPagePrivate(page);
 	unlock_page(page);
-	spin_lock(&pool->lock);
+	mutex_lock(&pool->mutex);
 	if (PageHighMem(newpage)) {
 		list_add_tail(&newpage->lru, &pool->high_items);
 		pool->high_count++;
@@ -274,7 +274,7 @@ static int ion_page_pool_migrate(struct address_space *mapping,
 		list_add_tail(&newpage->lru, &pool->low_items);
 		pool->low_count++;
 	}
-	spin_unlock(&pool->lock);
+	mutex_unlock(&pool->mutex);
 	put_page(page);
 	return 0;
 }
